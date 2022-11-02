96adac1d690bcca1d61c6684e6d6331b74ccd1d6
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-02 11:40:10 -0300

    feat:output property

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Course} from '../model/course';
 
 @Component({
@@ -10,11 +10,17 @@ export class CourseCardComponent implements OnInit {
 
   @Input() course:Course;
 
-
+  @Output()courseSelected=new EventEmitter<Course>();
 
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
+  onCourseViewed() {
+    console.log("card-component - button clicked");
+    this.courseSelected.emit(this.course);
+  }
 }
