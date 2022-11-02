43ee38ec3e1c00626e7e788d856fdbc16b664c45
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-02 16:52:45 -0300

    PIPES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,16 @@ export class AppComponent {
   //LISTA DE CURSOS PARA SER ITERADA
   courses=COURSES;
 
+  title=COURSES[0].description;
+
+  price=9.99124124124124;
+
+  rate=0.67;
+
+  course=COURSES[0];
+
+  startDate=new Date(2000,0,1);
+
 
   onCourseSelected(course:Course) {
     console.log("App component - course selected",course);
